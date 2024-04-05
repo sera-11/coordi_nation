66bef8b94facbfced7896930428d5125b9d5a264
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-04-05 22:22:45 +0000

    migrated meeting minutes

--- app/models/meeting_minute.rb
+++ app/models/meeting_minute.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: meeting_minutes
+#
+#  id              :integer          not null, primary key
+#  meeting_date    :date
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  organization_id :integer
+#
 class MeetingMinute < ApplicationRecord
   has_rich_text :content
 end
