71210e21a7b2846806796aee77c2a73a3e61085f
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-04-05 21:11:54 +0000

    created organizations and migrated

---
+++ app/models/organization.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: organizations
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+class Organization < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_05_210754) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_05_211032) do
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
