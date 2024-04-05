ea8274fd7bd41c8b5e3f538e0c0c76dc9f4abc77
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-04-05 22:40:51 +0000

    event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -16,4 +16,9 @@
 #
 class Event < ApplicationRecord
   belongs_to :organization
+
+  validates :title, presence: true
+  validates :event_date, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
