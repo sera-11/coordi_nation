04ec43ec60b7f02fcf4c98513279daf611ef4899
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-04-11 05:02:40 +0000

    changed time zone to central, added conditionals to user logged in for nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   private
 
   def set_time_zone
-    Time.zone = current_user.time_zone
+    Time.zone = 'Central Time (US & Canada)'
   end
 end
