805d1a3922d2f33abd1aee6646bea554bf184a9c
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-04-22 20:07:27 +0000

    removed devise controllers and added method to application controller. user can sign up now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
   before_action :set_time_zone, if: :user_signed_in?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username])
+  end
 
   private
 
   def set_time_zone
     Time.zone = 'Central Time (US & Canada)'
   end
+
+
 end
